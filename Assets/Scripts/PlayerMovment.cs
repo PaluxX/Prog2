b262f7852d1e274a173f878ef375d60151e659d0
PaluxX <agus.palombo01@hotmail.com>
2025-03-26 17:14:08 -0300

    x

---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+
+    private float _speed = 5f;
+    private float _xAxis, _yAxis;
+    
+
+
+}
