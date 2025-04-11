dfb1b1b8fa23282fc80958f6a6676944c1c451ef
PaluxX <agus.palombo01@hotmail.com>
2025-04-11 01:37:15 -0300

    x
    x

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class PlayerMovment : MonoBehaviour
 {
 
-    private float _speed = 5f;
-    private float _xAxis, _yAxis;
-    
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _xAxis, _yAxis;
+        
 
 
 }
