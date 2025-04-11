b8582c385bea7222d931dc48d6b0801604098ba1
PaluxX <agus.palombo01@hotmail.com>
2025-04-11 02:09:23 -0300

    mov
    .

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -5,9 +5,34 @@ using UnityEngine;
 public class PlayerMovment : MonoBehaviour
 {
 
-    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _movSpeed = 5f;
     [SerializeField] private float _xAxis, _yAxis;
-        
+
+    private Vector3 _dir = Vector3.zero;
+
+
+    private void Update()
+    {
+
+        _dir.x = Input.GetAxisRaw("Horizontal");
+        _dir.z = Input.GetAxisRaw("Vertical");
+
+        if (_dir.sqrMagnitude != 0.0f)
+        {
+            Movement(_dir);
+        }
+
+    }
+
+    private void Movement(Vector3 dir)
+    {
+        transform.position += dir.normalized * _movSpeed * Time.deltaTime;
+    
+    
+    }
+
+
+    
 
 
 }
